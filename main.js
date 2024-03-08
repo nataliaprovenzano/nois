7940298997ca4302ccbb4d02edaca5edd4e7148c
nataliaprovenzano <160235305+nataliaprovenzano@users.noreply.github.com>
2024-03-08 09:06:52 -0300

    a

---
+++ main.js
@@ -0,0 +1,5 @@
+// alert('bom dia rapaziada grupo do tutu aqui' + 'e feliz dias das mulheres pra todas')
+var nomeUsuario = prompt('Qual seu nome???');
+var texto = document.querySelector('span');
+
+texto.textContent = nomeUsuario;
