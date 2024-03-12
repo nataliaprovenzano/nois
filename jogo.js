e08284977876cb8f82f295082b197e5b1224a44d
nataliaprovenzano <160235305+nataliaprovenzano@users.noreply.github.com>
2024-03-12 08:22:01 -0300

    a

--- jogo.js
+++ jogo.js
@@ -2,16 +2,10 @@ let numeroSecreto = 8;
 
 let chute = '';
 
-while(chute == ''){
-    chute = prompt("Escholha um número entre 0 a 10");
-}
-
-
 while(chute == null || chute != numeroSecreto){
     alert('Tente novamente:)');
     chute = prompt('Escolha um número entre 0 a 10');
 }
-
 
 if(numeroSecreto == chute){
     alert('Parabéns!!!! Você acertou :)')
