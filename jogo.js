fa9af189d8594ee31b841bc3e44b5fc8cc2fc84a
nataliaprovenzano <160235305+nataliaprovenzano@users.noreply.github.com>
2024-03-12 07:58:42 -0300

    ok

--- jogo.js
+++ jogo.js
@@ -3,3 +3,13 @@ let numeroSecreto = 8;
 let chute = prompt('Escolha um número entre 0 a 10.');
 
 
+
+
+if(numeroSecreto == chute){
+    alert('Parabéns!!!! VOcê acertou :)')
+    console.log('Parabéns, acertou mizera')
+} else{
+    alert('Que tristReza, errou :(');
+    console.log('Infelizmente, errou mizera')
+    
+}
