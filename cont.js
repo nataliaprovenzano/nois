4b2a1385a01340301aea9e21bd459e7172dd3231
nataliaprovenzano <160235305+nataliaprovenzano@users.noreply.github.com>
2024-04-10 09:33:50 -0300

    a

--- cont.js
+++ cont.js
@@ -1,9 +1,15 @@
 const botoes = document.querySelectorAll('.objetivos');
 console.log(botoes.length);
 
+
+
+
+
+
 for (let sas = 0; sas < botoes.length; sas++){
     console.log(sas);
     botoes[sas].onclick = function(){
+        for(let j = 0; j < botoes.length; j++)
         botoes[sas].classList.add('ativo');
        
     }
