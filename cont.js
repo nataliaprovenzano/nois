bc74b69b444877502e707fc8fa594d1b83f500a1
nataliaprovenzano <160235305+nataliaprovenzano@users.noreply.github.com>
2024-04-10 09:38:56 -0300

    a

--- cont.js
+++ cont.js
@@ -9,7 +9,9 @@ console.log(botoes.length);
 for (let sas = 0; sas < botoes.length; sas++){
     console.log(sas);
     botoes[sas].onclick = function(){
-        for(let j = 0; j < botoes.length; j++)
+        for(let j = 0; j < botoes.length; j++){
+            botoes[j].classList.remove('ativo');
+        }
         botoes[sas].classList.add('ativo');
        
     }
