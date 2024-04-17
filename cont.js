dce27ce1abcdfd7935002c8e1e1a7d1544b41b41
nataliaprovenzano <160235305+nataliaprovenzano@users.noreply.github.com>
2024-04-17 10:23:54 -0300

    a

--- cont.js
+++ cont.js
@@ -26,3 +26,7 @@ let horas = Math.floor (minutos/60);
 let dias = Math.floor(horas/24);
 
 contadores[0].textContent = dias + " dias " + horas + " horas " + minutos + " minutos " + segundos + " segundos ";
+
+segundos %= 60;
+minutos %= 60;
+horas %= 60;
