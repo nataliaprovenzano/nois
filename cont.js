652d489073dca50fe17f24f30ca31bf035f2df87
nataliaprovenzano <160235305+nataliaprovenzano@users.noreply.github.com>
2024-04-17 10:16:58 -0300

    a

--- cont.js
+++ cont.js
@@ -25,4 +25,4 @@ let minutos = Math.floor(segundos/60);
 let horas = Math.floor (minutos/60);
 let dias = Math.floor(horas/24);
 
-contadores[0].textContent = dias = " dias " + horas + " horas " + minutos + " minutos " + segundos + " segundos ";
+contadores[0].textContent = dias + " dias " + horas + " horas " + minutos + " minutos " + segundos + " segundos ";
