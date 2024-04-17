76f8abcb8791e46ee425be9d688ee8e20b6acdf9
nataliaprovenzano <160235305+nataliaprovenzano@users.noreply.github.com>
2024-04-17 09:32:13 -0300

    a

--- cont.js
+++ cont.js
@@ -15,7 +15,7 @@ for (let sas = 0; sas < botoes.length; sas++){
     } 
 }
 const contadores = document.querySelectorAll('.contador');
-const tempo0objetivo1 = new Date('2024-04-18T00:00:00');
+const tempo0objetivo1 = new Date('2024-04-19T00:00:00');
 let tempoAtual = new Date();
 
 contadores[0].textContent = 'Contagem';
