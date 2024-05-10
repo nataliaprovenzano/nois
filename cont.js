aca01ef506e4a28cb1bad870fe8605e7394925dc
nataliaprovenzano <160235305+nataliaprovenzano@users.noreply.github.com>
2024-05-10 09:04:06 -0300

    a

--- cont.js
+++ cont.js
@@ -16,19 +16,30 @@ for (let sas = 0; sas < botoes.length; sas++){
 }
 const contadores = document.querySelectorAll('.contador');
 const tempo0objetivo1 = new Date('2024-04-19T00:00:00');
+const tempoObjetivo2 = new Date('2024-12-10T00:00:00');
+const tempoObjetivo3 = new Date('2025-01-15T00:00:00');
+const tempoObjetivo4 = new Date('2024-12-13T00:00:00');
 
 
-let tempoAtual = new Date();
-let tempoFinal = tempo0objetivo1 - tempoAtual;
+let tempos = [tempoObjetivo1, tempoObjetivo2, tempoObjetivo3, tempoObjetivo4];
+for(let i = 0; i< contadores.length;i++){
+contadores[i].textContent = CalculaTempo(tempos[i]);
+}
 
-let segundos = Math.floor(tempoFinal/1000);
-let minutos = Math.floor(segundos/60);
-let horas = Math.floor (minutos/60);
-let dias = Math.floor(horas/24);
+
+function CalculaTempo(tempoObjetivo) {
+    let tempoAtual = new Date();
+    let tempoFinal = tempoObjetivo - tempoAtual;
+    let segundos = Math.floor(tempoFinal/1000);
+    let minutos =  Math.floor(segundos/60);
+    let hora =  Math.floor(minutos/60);
+    let dia =  Math.floor(hora/24);
+
 
 segundos %= 60;
 minutos %= 60;
 horas %= 24;
 
-contadores[0].textContent = dias + " dias " + horas + " horas " + minutos + " minutos " + segundos + " segundos ";
+return dia + " Dias " + hora + " Horas " + minutos + " Minutos " + segundos + " Segundos ";
+}
 
